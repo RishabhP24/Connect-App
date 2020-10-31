dc4cd363190dc88ca3aff649196cdc93620f907c
RishabhP24 <73388699+RishabhP24@users.noreply.github.com>
2020-10-31 15:05:23 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -3,4 +3,6 @@ print('my first pull request') #Trying Github Pull request 1st time
 print('my first pull request') #Trying Github Pull request 2nd time
 print('this is my second try')
 print('this is my third try')
-print('making github fool')
+#lets try input statement
+i=int(input())
+print(i)
